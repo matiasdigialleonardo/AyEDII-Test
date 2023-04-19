df90a1a28351f1f3f490c3b9ef97c7d04ef9eaf0
Matias Di Gialleonardo <matiasdigialleonardo@gmail.com>
2023-04-19 16:18:26 -0300

    Commit en ramaAyEDII

--- uno.cpp
+++ uno.cpp
@@ -6,6 +6,7 @@ int main()
 {
 	// Agrego comentario para realizar un git pull.
 	// Comentario para modificar la rama.
+	// Comentario para crear la segunda rama creada que tiene el mismo nombre que la primera (ramaAyEDII).
 	std::cout << "Hello World!";
 	return 0;
 }
